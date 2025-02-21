27a56c97aad4b9a181f321f04be47442b3c036cf
Erik Gaasedelen <erikgaas@gmail.com>
2025-02-20 23:31:09 -0700

    remove added line

--- toolslm/md_hier.py
+++ toolslm/md_hier.py
@@ -142,6 +142,5 @@ Admin users management.
     test_no_content()
     test_different_levels()
     test_parent_includes_subheadings()
-
     test_multiple_level2_siblings()
     print('tests passed')
