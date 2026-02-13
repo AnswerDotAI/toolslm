ee0ec1039b1600a74763e7b1a9cd53d852b4559b
Kerem Turgutlu <keremturgutlu@gmail.com>
2026-02-13 18:51:30 +0300

    handle object type hint

--- toolslm/funccall.py
+++ toolslm/funccall.py
@@ -63,6 +63,7 @@ def _handle_type(t, defs):
     if t is NoneType: return {'type': 'null'}
     if t in custom_types: return {'type':'string', 'format':t.__name__}
     if ot is dict: return {'type': _types(t)[0]} 
+    if t is object: return {'type': 'object'}
     if ot in (list, tuple, set): return {'type': _types(t)[0], 'items':{}}
     if isinstance(t, type) and not issubclass(t, (int, float, str, bool)) or inspect.isfunction(t):
         defs[t.__name__] = _get_nested_schema(t)
